b0626b2e64a4d64ae7f2e74e35298b59c9ce83a2
phungvanphib1 <phungvanphib1@gmail.com>
2022-11-17 20:55:01 +0700

    alet

--- resources/views/admin/user/profile.blade.php
+++ resources/views/admin/user/profile.blade.php
@@ -74,6 +74,12 @@
                                         </div>
                                     </section>
                                 </div>
+                                <div class="social-links mt-2">
+                                    <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                                    <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                                    <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                                  </div>
 
                                 <div class="col-sm-9 com-w3ls">
                                     <section class="panel">
